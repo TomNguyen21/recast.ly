30f8866c60d93fd97f6564339a95f9fd6c9d7f2b
tom <tommy.nguyen0721@gmail.com>
2020-07-16 15:30:59 -0700

    Refactor VideoListEntry

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import Search from './components/Search.js';
 var App = () => (
   <div>
     <nav className="navbar">
@@ -15,6 +17,7 @@ var App = () => (
     </div>
   </div>
 );
+// import Search from './components/Search.js';
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,15 +1,27 @@
-var VideoListEntry = () => (
-  <div className="video-list-entry media">
-    <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/1w8Z0UOXVaY/default.jpg" alt="" />
-    </div>
-    <div className="media-body">
-      <div className="video-list-entry-title">Video Title</div>
-      <div className="video-list-entry-detail">Video Description</div>
-    </div>
-  </div>
-);
+// Import the {Components} module from react
+import React, {Component} from 'react';
 
+// Create a VideoListEntry class componenet that extends from React.Component
+class VideoListEntry extends Component {
+  constructor(props) {
+    super(props);
+  }
+  // render the the below HTML into React readable format
+  render() {
+    return (
+      <div className="video-list-entry media">
+        <div className="media-left media-middle">
+          <img className="media-object" src={props.source} alt="" />
+        </div>
+        <div className="media-body">
+          <div className="video-list-entry-title">{props.title}</div>
+          <div className="video-list-entry-detail">{props.description}</div>
+        </div>
+      </div>
+    );
+
+  }
+}
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoListEntry.propTypes = {
