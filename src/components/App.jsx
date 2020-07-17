7c6d0cce6fa46543d133bc291f5530dd22e231fb
tom <tommy.nguyen0721@gmail.com>
2020-07-16 22:55:44 -0700

    Stuck on state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,37 @@
-// import React from 'react';
-// import Search from './components/Search.js';
-// import VideoList from './VideoList.js';
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
+import Search from './Search.js';
+// import exampleVideoData from '../exampleVideoData';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+  handleVideoClick(video) {
+    this.setState({currentVideo: video});
+  }
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><h5><VideoPlayer video={this.state.currentVideo} /></h5></div>
+          </div>
+          <div className="col-md-5">
+            <div><h5><VideoList videos={this.state.videos}/></h5></div>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 // import Search from './components/Search.js';
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
